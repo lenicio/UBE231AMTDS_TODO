22736991c6ea68f299da61d41ee94af0f8da4284
Lenício Jr <lenicio.junior@hotmail.com>
2023-12-08 08:47:22 -0300

    Função Adicionar Todo

--- script/app.js
+++ script/app.js
@@ -1,25 +1,24 @@
 let todoLista = [];
-let novaLi = "";
+let botaoAdicionar = document.getElementById("button-adicionar");
+botaoAdicionar.addEventListener("click", adicionaItem);
 
-// Adiciona elementos na lista
-todoLista.push("Estudar CSS");
-todoLista.push("Estudar HTML");
-todoLista.push("Estudar JavaScript");
-todoLista.push("Estudar PHP");
-todoLista.push("Estudar Python");
-todoLista.push("Estudar Java");
-
-// Remove elementos da lista
-todoLista.splice(1, 1);
-
-todoLista.forEach(function (item) {
-  novaLi += `
+function exibeLista() {
+  let novaLi = "";
+  todoLista.forEach(function (item) {
+    novaLi += `
     <li class="todo-item">
     <i class="fa-solid fa-circle-check"></i>
     <p class="">${item}</p>
     <i class="fa-solid fa-trash"></i>
     </li>
 `;
-});
+  });
+  document.querySelector(".todo-lista").innerHTML = novaLi;
+}
 
-document.querySelector(".todo-lista").innerHTML = novaLi;
+function adicionaItem() {
+  let inputAtividade = document.getElementById("input-atividade").value;
+  todoLista.push(inputAtividade);
+  document.getElementById("input-atividade").value = "";
+  exibeLista();
+}
